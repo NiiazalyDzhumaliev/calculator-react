1a8c72d9d7199991fc8266aa6da9c6e01caac06d
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-12-08 18:40:34 +0600

    Add App functional component

---
+++ src/components/App.js
@@ -0,0 +1,13 @@
+import Display from 'src/components/Display';
+import ButtonPanel from 'src/components/ButtonPanel';
+
+function App() {
+  return (
+    <div>
+      <Display />
+      <ButtonPanel />
+    </div>
+  );
+}
+
+export default App;
