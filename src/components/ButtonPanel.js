65dd38885b3b8158787c56e905884174181e1d5e
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-12-09 15:26:48 +0600

    Add React fragments

--- src/components/ButtonPanel.js
+++ src/components/ButtonPanel.js
@@ -1,37 +1,64 @@
 import Button from './Button';
 
+const buttonGroups = {
+  groupOne: {
+    id: 1,
+    group: ['AC', '+/-', '%', '÷'],
+  },
+
+  groupTwo: {
+    id: 2,
+    group: ['7', '8', '9', 'X'],
+  },
+
+  groupThree: {
+    id: 3,
+    group: ['4', '5', '6', '-'],
+  },
+
+  groupFour: {
+    id: 4,
+    group: ['1', '2', '3', '+'],
+  },
+
+  groupFive: {
+    id: 5,
+    group: ['0', '.', '='],
+  },
+};
+
 const ButtonPanel = () => (
-  <div className="btn-panel">
-    <div className="group-one">
-      <Button name="AC" />
-      <Button name="+/-" />
-      <Button name="%" />
-      <Button name="÷" />
+  <>
+    <div className="groupOne">
+      {buttonGroups.groupOne.group.map(button => (
+        <Button name={button} key={buttonGroups.groupOne.id} />
+      ))}
     </div>
-    <div className="group-two">
-      <Button name="7" />
-      <Button name="8" />
-      <Button name="9" />
-      <Button name="X" />
+
+    <div className="groupTwo">
+      {buttonGroups.groupTwo.group.map(button => (
+        <Button name={button} key={buttonGroups.groupTwo.id} />
+      ))}
     </div>
-    <div className="group-three">
-      <Button name="4" />
-      <Button name="5" />
-      <Button name="6" />
-      <Button name="-" />
+
+    <div className="groupThree">
+      {buttonGroups.groupThree.group.map(button => (
+        <Button name={button} key={buttonGroups.groupThree.id} />
+      ))}
     </div>
-    <div className="group-four">
-      <Button name="1" />
-      <Button name="2" />
-      <Button name="3" />
-      <Button name="+" />
+
+    <div className="groupFour">
+      {buttonGroups.groupFour.group.map(button => (
+        <Button name={button} key={buttonGroups.groupFour.id} />
+      ))}
     </div>
-    <div className="group-five">
-      <Button name="0" />
-      <Button name="." />
-      <Button name="=" />
+
+    <div className="groupFive">
+      {buttonGroups.groupFive.group.map(button => (
+        <Button name={button} key={buttonGroups.groupFive.id} />
+      ))}
     </div>
-  </div>
+  </>
 );
 
 export default ButtonPanel;
