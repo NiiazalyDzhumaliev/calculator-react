161687f12d6b5bf075e87abf89ffafd41730d180
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2020-12-15 19:14:25 +0600

    Add render and return

--- src/components/App.js
+++ src/components/App.js
@@ -1,13 +1,43 @@
+import React from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-const App = () => (
-  <div>
-    <Display />
-    <ButtonPanel name={calculate} />
-  </div>
-);
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      total: null,
+      next: null,
+      operation: null,
+    };
+
+    this.handleClick = this.handleClick.bind(this);
+  }
+
+  handleClick(buttonName) {
+    const { total, next, operation } = this.state;
+    this.setState(calculate({ total, next, operation }, buttonName));
+  }
+
+  render() {
+    const { total, next, operation } = this.state;
+    let result = total || next;
+
+    if (total && next && operation) {
+      result = `${total} ${operation} ${next}`;
+    } else if (total && operation) {
+      result = `${total} ${operation}`;
+    }
+
+    return (
+      <div>
+        <Display calcResult={result || undefined} />
+        <ButtonPanel clickHandler={this.handleClick} />
+      </div>
+    );
+  }
+}
 
 export default App;
