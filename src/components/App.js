2c7a9d4c28c48748c2091e2a5d0f03fb2eee0f53
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-06 18:16:06 +0600

    Refactor code

--- src/components/App.js
+++ src/components/App.js
@@ -1,43 +1,36 @@
-import React from 'react';
+import React, { useState } from 'react';
 import Display from './Display';
 import ButtonPanel from './ButtonPanel';
 import '../App.css';
 import calculate from '../logic/calculate';
 
-class App extends React.Component {
-  constructor(props) {
-    super(props);
-    this.state = {
-      total: null,
-      next: null,
-      operation: null,
-    };
+const App = () => {
+  const [state, setState] = useState({
+    total: null,
+    next: null,
+    operation: null,
+  });
 
-    this.handleClick = this.handleClick.bind(this);
+  const handleClick = buttonName => {
+    const { total, next, operation } = state;
+    setState(calculate({ total, next, operation }, buttonName));
+  };
+
+  const { total, next, operation } = state;
+  let result = total || next;
+
+  if (total && next && operation) {
+    result = `${total} ${operation} ${next}`;
+  } else if (total && operation) {
+    result = `${total} ${operation}`;
   }
 
-  handleClick(buttonName) {
-    const { total, next, operation } = this.state;
-    this.setState(calculate({ total, next, operation }, buttonName));
-  }
-
-  render() {
-    const { total, next, operation } = this.state;
-    let result = total || next;
-
-    if (total && next && operation) {
-      result = `${total} ${operation} ${next}`;
-    } else if (total && operation) {
-      result = `${total} ${operation}`;
-    }
-
-    return (
-      <div>
-        <Display calcResult={result || undefined} />
-        <ButtonPanel clickHandler={this.handleClick} />
-      </div>
-    );
-  }
-}
+  return (
+    <div>
+      <Display calcResult={result || undefined} />
+      <ButtonPanel clickHandler={handleClick} />
+    </div>
+  );
+};
 
 export default App;
