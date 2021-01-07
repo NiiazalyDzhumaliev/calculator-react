9523102cfeebe000460382ec8aea5bff4134747c
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-07 14:57:13 +0600

    Pass color props

--- src/components/Button.js
+++ src/components/Button.js
@@ -1,17 +1,27 @@
 import PropTypes from 'prop-types';
 
 const Button = props => {
-  const { name } = props;
+  const { name, color } = props;
   const handleClick = e => props.clickHandler(e.target.innerText);
   return (
-    <button type="button" className="button" onClick={handleClick}>
+    <button
+      type="button"
+      className="button"
+      onClick={handleClick}
+      style={{ backgroundColor: color }}
+    >
       {name}
     </button>
   );
 };
 
+Button.defaultProps = {
+  color: 'orange',
+};
+
 Button.propTypes = {
   name: PropTypes.string.isRequired,
+  color: PropTypes.string,
   clickHandler: PropTypes.func.isRequired,
 };
 
