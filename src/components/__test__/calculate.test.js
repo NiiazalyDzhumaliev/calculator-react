9198d73c453e2a4807e6185acd363f90da1eacce
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-12 17:02:56 +0600

    Test number buttons

--- src/components/__test__/calculate.test.js
+++ src/components/__test__/calculate.test.js
@@ -1,6 +1,6 @@
 import calculate from '../../logic/calculate';
 
-test('+/- button', () => {
+test('+/- button on initial', () => {
   const button = '+/-';
   const obj = {
     total: null,
@@ -14,7 +14,7 @@ test('+/- button', () => {
   });
 });
 
-test('+/- button', () => {
+test('+/- button on total', () => {
   const button = '+/-';
   const obj = {
     total: 5,
@@ -24,6 +24,20 @@ test('+/- button', () => {
   expect(calculate(obj, button)).toEqual({
     total: '-5',
     next: null,
+    operation: null,
+  });
+});
+
+test('number buttons', () => {
+  const button = '5';
+  const obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: null,
+    next: '5',
     operation: null,
   });
 });
