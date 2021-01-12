da8303b5bfdcdd98e8337785c53a38fc74797d9c
Dzhumaliev Niiazaly <diamondas1993@gmail.com>
2021-01-12 18:12:41 +0600

    Add tests on operands

--- src/components/__test__/calculate.test.js
+++ src/components/__test__/calculate.test.js
@@ -41,3 +41,73 @@ test('number buttons', () => {
     operation: null,
   });
 });
+
+test('. button', () => {
+  const button = '.';
+  const obj = {
+    total: null,
+    next: '5',
+    operation: null,
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: null,
+    next: '5.',
+    operation: null,
+  });
+});
+
+test('AC button', () => {
+  const button = 'AC';
+  const obj = {
+    total: null,
+    next: null,
+    operation: null,
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: null,
+    next: null,
+    operation: null,
+  });
+});
+
+test('= button', () => {
+  const button = '=';
+  const obj = {
+    total: 5,
+    next: 5,
+    operation: '+',
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: '10',
+    next: null,
+    operation: null,
+  });
+});
+
+test('operation button', () => {
+  const button = '+';
+  const obj = {
+    total: 5,
+    next: 5,
+    operation: null,
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: '10',
+    next: null,
+    operation: '+',
+  });
+});
+
+test('% button', () => {
+  const button = '%';
+  const obj = {
+    total: null,
+    next: 100,
+    operation: null,
+  };
+  expect(calculate(obj, button)).toEqual({
+    total: '1',
+    next: null,
+    operation: null,
+  });
+});
